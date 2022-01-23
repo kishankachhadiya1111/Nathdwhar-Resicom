1bf0e19b83a561528d2de1710f1e1d566fca970c
Kishan Kachhadiya <kishan.logicwind@gmail.com>
2022-01-23 13:14:35 +0530

    updated ui

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 (function ($) {
     "use strict";
-    
     // Initiate the wowjs
     new WOW().init();
-    
-    
-    // Back to top button
+
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
+        const scrollPosition = $(this).scrollTop() || 1;
+        if (scrollPosition <= 738) {
+            const imageSize = 100 - ((100 / 738) * scrollPosition);
+            $('.main-screen a').css("height", `${imageSize}vh`);
+            $('.main-screen a').css("marginTop", `${scrollPosition}px`);
+            $('.main-screen a').css("opacity", `${imageSize / 100}`);
         }
-    });
+    })
+    // Back to top button
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 200) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 20000, 'easeInOutExpo');
         return false;
     });
-    
-    
+
     // Sticky Navbar
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 90) {
+        if ($(this).scrollTop() > 738 - 300) {
+            $('.nav-bar').addClass('d-block');
+        } else {
+            $('.nav-bar').removeClass('d-block');
+        }
+        if ($(this).scrollTop() > 738 - 300) {
             $('.nav-bar').addClass('nav-sticky');
             $('.carousel, .page-header').css("margin-top", "73px");
         } else {
@@ -29,8 +41,8 @@
             $('.carousel, .page-header').css("margin-top", "0");
         }
     });
-    
-    
+
+
     // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
@@ -47,15 +59,15 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
+
+
     // jQuery counterUp
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
-    
-    
+
+
     // Modal Video
     $(document).ready(function () {
         var $videoSrc;
@@ -93,33 +105,33 @@
         slidesToShow: 3,
         asNavFor: '.testimonial-slider'
     });
-    $('.testimonial .slider-nav').css({"position": "relative", "height": "160px"});
-    
-    
+    $('.testimonial .slider-nav').css({ "position": "relative", "height": "160px" });
+
+
     // Blogs carousel
     $(".related-slider").owlCarousel({
         autoplay: true,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="fa fa-angle-left" aria-hidden="true"></i>',
             '<i class="fa fa-angle-right" aria-hidden="true"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             }
         }
     });
-    
-    
+
+
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
@@ -130,8 +142,8 @@
         $("#portfolio-flters li").removeClass('filter-active');
         $(this).addClass('filter-active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
-    
+
 })(jQuery);
 
