7c59115fbf71ebd6c2f9713ebe32f2c608fd5ef3
Kishan Kachhadiya <kishan.logicwind@gmail.com>
2022-01-26 10:59:37 +0530

    add some ui portion

--- js/main.js
+++ js/main.js
@@ -27,13 +27,13 @@
         }
     })
     // Back to top button
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 200) {
-    //         $('.back-to-top').fadeIn('slow');
-    //     } else {
-    //         $('.back-to-top').fadeOut('slow');
-    //     }
-    // });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
 
     $('.back-to-top').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 20000, 'easeInOutExpo');
